0887f9bd20af953bee9fb212ec988f73817ef81e
mariam <mariam@gamil.com>
2026-02-04 07:08:00 +0200

    hhhheeeeeeeeeeeeeeeeeeeeeeeeeeh

---
+++ lib/core/theming/app_colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color blue = Color(0xff3669C9);
+  static const Color red = Color(0xffED1010);
+}
---
+++ lib/core/theming/app_fonts.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AppFonts {
+  final TextStyle font32w600 = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+  );
+
+  final TextStyle font24w600 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+
+  final TextStyle font16w600 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+
+  final TextStyle font14w600 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+  );
+
+  final TextStyle font16w400 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+
+  final TextStyle font16w500 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+
+  final TextStyle font14w500 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+}
